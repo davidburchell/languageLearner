e119ec0be5d554943e3ef8d2d281bcad68f70018
david burchell <david.b.burchell@gmail.com>
2026-01-20 21:57:34 -0600

    main logic

--- AppMain.java
+++ AppMain.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppMain {
+
+    public static void main(String [] args){
+
+        System.out.println("================");
+        System.out.println("LANGUAGE LEARNER");
+        System.out.println("================");
+
+        List<String> fileNames = new ArrayList<>();
+        fileNames.add("unitOneWords.txt");
+
+        List<Term> termList = Utility.readTermsFromFiles(fileNames, "SEE_FOREIGN");
+        LanguageLearner languageLearner = new LanguageLearner(10, 6, 2, termList);
+        languageLearner.simulateClassroom();
+
+        System.out.println();
+        System.out.println("END OF LESSON");
+    }
 }
